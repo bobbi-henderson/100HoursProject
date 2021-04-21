7077373896aab5bd740faf86c4b93f175b0e87af
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-04-20 20:52:02 -0500

    initial commit, set up server and database connection, started to work on home page layout

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+const PORT = 2121
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', homeRoutes)
+ 
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})   
